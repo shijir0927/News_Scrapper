b867bb92b81cb8b3602c2380fd16c0d6b5bbd74f
shijir0927 <shijiree_mng@yahoo.com>
2019-11-12 13:35:01 -0800

    route

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,13 +1,12 @@
-let path = require ('path')
-module.exports = function(app){
-    // homepage route
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname + "/../public/index.html"))
-})
+let path = require("pat");
+module.exports = function(app) {
+  // homepage route
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../public/index.html"));
+  });
 
-// saved articles page route
-app.get("/saved",(req,res) => {
-    res.sendFile(path.join(__dirname + "/../public/saved.html"))
-})
-
-}
+  // saved articles page route
+  app.get("/saved", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../public/saved.html"));
+  });
+};
